baaa807565b9cc246c08333109ae212fd185b3cf
BillMcBlackjack <fabrilongo407@gmail.com>
2023-12-29 19:01:28 +0100

    add 2 function to toggle and change background color

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 // UI
-
+const ROWS = 16;
+const COLUMNS = 16;
+let enableHover = false;
 const gridContainer = document.querySelector('#grid-container');
 
-for (let i = 0; i < 16; i++) {
-  for (let k = 0; k < 16; k++) {
+for (let row = 0; row < ROWS; row++) {
+  for (let column = 0; column < COLUMNS; column++) {
     const square = document.createElement('div');
     square.classList.add('cell');
-    
     gridContainer.appendChild(square);
+
+    square.addEventListener('click', function() {
+      enableHover = !enableHover;
+      // makes sure that the cell clicked changes it's color
+      this.style.backgroundColor = 'black';
+    });
+    square.addEventListener('mouseover', function() {
+      if (enableHover) {
+      this.style.backgroundColor = 'black';
+      }
+    })
+
   }
 }
